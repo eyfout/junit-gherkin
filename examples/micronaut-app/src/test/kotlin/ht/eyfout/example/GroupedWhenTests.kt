882757b7a22431a13b6f33b3372f924785d5cfc3
Sidney Milien <sidney.milien@protonmail.com>
2024-09-29 15:10:40 -0400

    test: rename test files

---
+++ examples/micronaut-app/src/test/kotlin/ht/eyfout/example/GroupedWhenTests.kt
@@ -0,0 +1,50 @@
+package ht.eyfout.example
+
+import ht.eyfout.example.client.DMVClient
+import ht.eyfout.example.http.ControllerAPI
+import ht.eyfout.example.http.ExampleStateScopeProvider
+import ht.eyfout.junit.jupiter.api.GherkinDynamicTest
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.HttpStatus
+import io.micronaut.test.annotation.MockBean
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import io.mockk.mockk
+import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.TestFactory
+
+@MicronautTest
+class GroupedWhenTests {
+
+    @MockBean(DMVClient::class)
+    val dmvClient: DMVClient = mockk()
+
+    @Inject
+    lateinit var provider: ExampleStateScopeProvider
+
+    @TestFactory
+    fun grouped() = GherkinDynamicTest
+        .dynamicTest(provider)
+        .given("an unauthorized request") {
+            it.GETManufacturerAnswer(null) {
+                HttpResponse.unauthorized()
+            }
+            it.GETVehiclesAnswer(null, null) {
+                HttpResponse.unauthorized()
+            }
+        }.`when`("calling service api") {
+            it.httpRequest(ControllerAPI.VehiclesByManufacturerName)
+                .queryParam("make", "Nissan")
+                .header("Authorization", "other")
+
+            it.httpRequest(ControllerAPI.VehiclesByManufacturerID)
+                .header("Authorization", "other")
+                .pathParam("manufacturerID", "Nissan-#1")
+
+            it.httpRequest(ControllerAPI.Manufacturers)
+                .header("Authorization", "other")
+        }.then("Http 401") {
+            assertEquals(HttpStatus.UNAUTHORIZED.code, it.httpResponse().statusCode())
+        }
+
+}
