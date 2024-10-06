fccc5d18164e6995165e394867384f7c1c3607f7
Sidney Milien <sidney.milien@protonmail.com>
2024-10-06 10:33:14 -0400

    feat: add sourceSet for plugin

--- plugins/junit-gherkin-http/src/main/java/ht/eyfout/plugins/junit/jupiter/gherkin/GherkinHttpCodeGeneratorPlugin.java
+++ plugins/junit-gherkin-http/src/main/java/ht/eyfout/plugins/junit/jupiter/gherkin/GherkinHttpCodeGeneratorPlugin.java
@@ -1,42 +1,52 @@
 package ht.eyfout.plugins.junit.jupiter.gherkin;
 
-import org.gradle.api.*;
+import org.gradle.api.Action;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.file.Directory;
 import org.gradle.api.provider.Provider;
+import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.SourceSetContainer;
 import org.gradle.api.tasks.TaskProvider;
+import org.jetbrains.annotations.NotNull;
 
 import javax.inject.Inject;
 
 public class GherkinHttpCodeGeneratorPlugin implements Plugin<Project> {
+    private final String ROOT_DIR = "/generated/junit-gherkin/";
+
     @Inject
     public GherkinHttpCodeGeneratorPlugin() {
 
     }
 
-    String captitalize(String str){
-        return str.substring(0,1).toUpperCase() + str.substring(1);
+    String captitalize(String str) {
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
     @Override
     public void apply(Project project) {
-        SourceSetContainer sourceSets = (SourceSetContainer) project.getProperties().get("sourceSets");
-        sourceSets.all(sourceSet -> {
-            Provider<Directory> outputDir = project.getLayout().getBuildDirectory().dir("generated/junit-gherkin/" + sourceSet.getName() + "/classes");
-            TaskProvider<GherkinHttpCodeGenerationTask> task = project.getTasks().register("gherkinHttpAPICodeGen" + captitalize(sourceSet.getName()),
-                    GherkinHttpCodeGenerationTask.class, it -> {
-                        it.setGroup("Code Generation");
-                        it.setDescription("Generates Java bytecode representation of the OpenAPI.");
-                        it.setOutputDir(outputDir);
-                        it.setSourceSet(sourceSet.getName());
-//                        it.setSpecsDir(sourceSet.getResources().srcDir(""));
-                    });
-            project.getTasks().named(sourceSet.getCompileJavaTaskName()).configure(new Action<Task>() {
-                @Override
-                public void execute(Task compileJavaTask ) {
-                    compileJavaTask.dependsOn(task);
-                }
-            });
+        SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
+        SourceSet mainSourceSet = sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME);
+        Provider<Directory> outputDir = project.getLayout().getBuildDirectory().dir(ROOT_DIR + mainSourceSet.getName() + "/classes");
+        mainSourceSet.getOutput().dir(outputDir);
+        TaskProvider<GherkinHttpCodeGenerationTask> task = project.getTasks().register("httpAPICodeGen",
+                GherkinHttpCodeGenerationTask.class, it -> {
+                    it.setGroup("Code Generation");
+                    it.setDescription("Generates bytecode that conforms to junit-gherkin's HTTP APIs for an swagger document.");
+                    it.setOutputDir(outputDir);
+                    it.setSourceSet(mainSourceSet.getName());
+                });
+        project.getTasks().named(mainSourceSet.getCompileJavaTaskName()).configure(new Action<Task>() {
+            @Override
+            public void execute(@NotNull Task compileJavaTask) {
+                compileJavaTask.dependsOn(task);
+            }
         });
+
+        SourceSet pluginSourceSet = sourceSets.create("httpAPICodeGen");
+        pluginSourceSet.getResources().srcDir(project.getLayout().getBuildDirectory().dir(ROOT_DIR + mainSourceSet.getName() + "/java"));
+        pluginSourceSet.getOutput().dir(outputDir);
     }
 }
